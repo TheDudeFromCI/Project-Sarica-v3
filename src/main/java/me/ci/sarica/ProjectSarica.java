ce551343457b8a97a9950b9966f2b12fbe0940a4
TheDudeFromCI <thedudefromci@gmail.com>
2017-10-12 15:21:10 -0700

    Created project.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

---
+++ src/main/java/me/ci/sarica/ProjectSarica.java
@@ -0,0 +1,15 @@
+package me.ci.sarica;
+
+/**
+ * Created by thedudefromci on 10/9/17.
+ *
+ * This class is for the purpose of starting the simulation, along with processing any program
+ * arguments that could effect the simulation.
+ */
+public class ProjectSarica
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
